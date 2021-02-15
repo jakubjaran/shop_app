b47b4cba84bc2ce41b5e619d96db539607ce1e23
unknown <jakubjaran@gmail.com>
2021-02-15 11:47:14 +0100

    Product details screen ui

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/badge.dart';
 import '../providers/products.dart';
+import '../providers/cart.dart';
+import '../screens/cart_screen.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/details-screen';
@@ -15,6 +18,79 @@ class ProductDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+        actions: [
+          Consumer<Cart>(
+            builder: (_, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child,
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Image.network(loadedProduct.imageUrl),
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      color: Theme.of(context).accentColor, width: 2),
+                ),
+                color: Colors.black38,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: Row(
+                children: [
+                  Text(
+                    loadedProduct.title,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${loadedProduct.price}',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Theme.of(context).accentColor,
+                  ),
+                  Consumer<Cart>(
+                    builder: (_, cart, child) => FlatButton(
+                      onPressed: () {
+                        cart.addItem(loadedProduct.id, loadedProduct.title,
+                            loadedProduct.price);
+                      },
+                      child: child,
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                    child: Text(
+                      '+ ADD TO CART',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
