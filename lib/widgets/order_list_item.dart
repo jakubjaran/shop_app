d336c7a75ffb6a16698a65fc1ca7c5842483d682
unknown <jakubjaran@gmail.com>
2021-02-16 01:38:26 +0100

    Expandable order list item

--- lib/widgets/order_list_item.dart
+++ lib/widgets/order_list_item.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' show Order;
 
-class OrderListItem extends StatelessWidget {
+class OrderListItem extends StatefulWidget {
   final Order order;
 
   OrderListItem(this.order);
+
+  @override
+  _OrderListItemState createState() => _OrderListItemState();
+}
+
+class _OrderListItemState extends State<OrderListItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,21 +28,48 @@ class OrderListItem extends StatelessWidget {
       ),
       margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-      child: ListTile(
-        leading: Chip(
-          label: Text(
-            '\$${order.amount.toStringAsFixed(2)}',
-            style: TextStyle(color: Colors.white),
+      child: Column(
+        children: [
+          ListTile(
+            leading: Chip(
+              label: Text(
+                '\$${widget.order.amount.toStringAsFixed(2)}',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Theme.of(context).accentColor,
+            ),
+            title: Text(
+              DateFormat('dd/MM/yyyy | hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
           ),
-          backgroundColor: Theme.of(context).accentColor,
-        ),
-        title: Text(
-          DateFormat('dd/MM/yyyy | hh:mm').format(order.date),
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: () {},
-        ),
+          if (_expanded)
+            Container(
+              height: min(widget.order.products.length * 20.0 + 30, 80),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+              child: ListView(
+                children: widget.order.products
+                    .map((prod) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(prod.title),
+                              Text('${prod.quantity}x \$${prod.price}'),
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              ),
+            )
+        ],
       ),
     );
   }
