c037780bb4b0feff6bb92695235e1b367e93fb6f
unknown <jakubjaran@gmail.com>
2021-02-21 21:29:53 +0100

    Basic http post request

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -51,6 +54,19 @@ class Products with ChangeNotifier {
   }
 
   void addItem(Product product) {
+    const url =
+        'https://shopapp-c2c88-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
+
     final newProduct = Product(
       title: product.title,
       description: product.description,
