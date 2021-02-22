a7cb449a5689b71cf1ce91a8f6ebe7469e767c5b
unknown <jakubjaran@gmail.com>
2021-02-22 23:11:26 +0100

    Error dialog

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -100,7 +100,22 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addItem(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Ooops...'),
+                  content: Text('Sorry, something went wrong!'),
+                  actions: [
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                      child: Text('OKAY'),
+                    ),
+                  ],
+                ));
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
