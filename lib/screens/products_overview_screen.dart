cb75d0f9634175dd2e43d397749e404419f53643
unknown <jakubjaran@gmail.com>
2021-02-23 10:35:27 +0100

    Set products data, show spinner while loading

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -23,11 +23,20 @@ class ProductsOverviewScreen extends StatefulWidget {
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showFavoritesOnly = false;
   var _isInit = true;
+  var _isLoading = false;
 
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      Provider.of<Products>(context).fetchAndSetItems();
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetItems().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+      _isInit = false;
     }
     super.didChangeDependencies();
   }
@@ -75,7 +84,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showFavoritesOnly),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showFavoritesOnly),
     );
   }
 }
