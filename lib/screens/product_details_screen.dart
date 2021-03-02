3d08738b1f832d5b7094f73ea763ebeaa8e204a0
unknown <jakubjaran@gmail.com>
2021-03-02 11:52:13 +0100

    Using slivers

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -16,84 +16,80 @@ class ProductDetailsScreen extends StatelessWidget {
     final loadedProduct = Provider.of<Products>(context).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-        actions: [
-          Consumer<Cart>(
-            builder: (_, cart, child) => Badge(
-              value: cart.itemsCount.toString(),
-              child: child,
-            ),
-            child: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {
-                Navigator.of(context).pushNamed(CartScreen.routeName);
-              },
-            ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              child: Hero(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 300,
+            title: Text(loadedProduct.title),
+            actions: [
+              Consumer<Cart>(
+                builder: (_, cart, child) => Badge(
+                  value: cart.itemsCount.toString(),
+                  child: child,
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartScreen.routeName);
+                  },
+                ),
+              ),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              background: Hero(
                 tag: loadedProduct.id,
                 child: Image.network(loadedProduct.imageUrl),
               ),
-              width: double.infinity,
-              height: 300,
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                      color: Theme.of(context).accentColor, width: 2),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      loadedProduct.title,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Spacer(),
+                    Chip(
+                      label: Text(
+                        '\$${loadedProduct.price}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      backgroundColor: Theme.of(context).accentColor,
+                    ),
+                    Consumer<Cart>(
+                      builder: (_, cart, child) => FlatButton(
+                        onPressed: () {
+                          cart.addItem(loadedProduct.id, loadedProduct.title,
+                              loadedProduct.price);
+                        },
+                        child: child,
+                        textColor: Theme.of(context).primaryColor,
+                      ),
+                      child: Text(
+                        '+ ADD TO CART',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
                 ),
-                color: Colors.black38,
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Row(
-                children: [
-                  Text(
-                    loadedProduct.title,
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Spacer(),
-                  Chip(
-                    label: Text(
-                      '\$${loadedProduct.price}',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    backgroundColor: Theme.of(context).accentColor,
-                  ),
-                  Consumer<Cart>(
-                    builder: (_, cart, child) => FlatButton(
-                      onPressed: () {
-                        cart.addItem(loadedProduct.id, loadedProduct.title,
-                            loadedProduct.price);
-                      },
-                      child: child,
-                      textColor: Theme.of(context).primaryColor,
-                    ),
-                    child: Text(
-                      '+ ADD TO CART',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                width: double.infinity,
+                child: Text(
+                  loadedProduct.description,
+                  softWrap: true,
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              width: double.infinity,
-              child: Text(
-                loadedProduct.description,
-                softWrap: true,
-              ),
-            )
-          ],
-        ),
+              SizedBox(height: 800),
+            ]),
+          ),
+        ],
       ),
     );
   }
